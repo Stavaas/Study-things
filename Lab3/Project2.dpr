d680b225e3f51f7e0ed5a65236fbd7a1437fc371
Stavaas <47716283+Stavaas@users.noreply.github.com>
2019-03-06 10:54:27 +0300

    Lab3 added

---
+++ Lab3/Project2.dpr
@@ -0,0 +1,40 @@
+program Project2;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  Unit1 in 'Unit1.pas';
+
+var Prop:Triangle;
+    l, sd:Integer;
+    ha, hb, hc:Integer;
+
+begin
+  Prop:=Triangle.Create( 3, 4, 5 );
+
+  Readln(l);
+  Prop.A:=l;
+
+  Readln(l);
+  Prop.B:=l;
+
+  Readln(l);
+  Prop.C:=l;
+  Writeln('Result - ');
+  if Prop.q=True then begin
+    Writeln(Prop.FA);
+    writeln(Prop.FB);
+    writeln(Prop.FC);
+  end
+  else
+    Writeln('Thats not a triangle');
+
+  if Prop.IsRectangular(Prop.FA, Prop.FB, Prop.FC) = True then
+    Writeln('true')
+  else
+    Writeln('false');
+
+  Prop.Free;
+  Readln;
+end.
